8013138f52b32633aaaa987b232b4a6d78115ade
Raquel Coriat <rcoriat14@gmail.com>
2018-11-05 02:31:25 -0400

    eliminar plato listo + cambios editar

--- ProyectoAngular/src/app/menuadmin/menuadmin.component.ts
+++ ProyectoAngular/src/app/menuadmin/menuadmin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
 import { FireService } from '../services/fire.service';
 import { AngularFireStorage } from '@angular/fire/storage';
 import { Plato } from '../models/plato';
@@ -19,6 +20,7 @@ export class MenuadminComponent implements OnInit {
   public nplato = {} as Plato; // nuevo plato
   uploaded: boolean = false;
   public eplato: Plato; // plato de edicion
+  modalRef: BsModalRef; //modal confirmar
 
   constructor(public platoService: FireService, private afStorage: AngularFireStorage) {
     this.uploaded = false;
@@ -50,25 +52,37 @@ export class MenuadminComponent implements OnInit {
   }
 
   addPlato() {
-
-    this.imagenURL.subscribe(params => {
-      this.nplato.imagen = params;
+    if(this.imagenURL == undefined){
+      this.nplato.imagen = "https://firebasestorage.googleapis.com/v0/b/umakeitbd.appspot.com/o/plato2.png?alt=media&token=3b1e37ab-b454-47ad-af29-e030aa44ae85";
       this.platoService.agregarPlato(this.nplato);
-    });
-
+    }
+    else{
+      this.imagenURL.subscribe(params => {
+        this.nplato.imagen = params;
+        this.platoService.agregarPlato(this.nplato);
+      });
+    }
   }
 
   editarPlato(evento, plato) {
+
     this.eplato = plato;
+
   }
+
+  borrarPlato(event, plato) {
+    this.platoService.borrarPlato(plato);
+  }
+
+
 
   actualizarPlato() {
     console.log('eplato antes de imagenURL: ');
     console.log(this.eplato);
-    if (this.imagenURL != null) {
+    if (this.imagenURL != undefined) {
       this.imagenURL.subscribe(params => {
       this.eplato.imagen = params;
-      //this.platoService.actualizarPlato(this.eplato);
+      this.platoService.actualizarPlato(this.eplato);
       console.log('eplato despues de imagenURL: ');
       console.log(this.eplato);
     });
@@ -79,6 +93,8 @@ export class MenuadminComponent implements OnInit {
     }
     this.eplato = {} as Plato;
   }
+
+
 
   limpiarNPlato() {
     this.nplato.cantidad = null;
